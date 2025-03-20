389e2b2c96ba656989a10377e8ddbc5be315a2b0
miroaccgh <wabysaby@proton.me>
2025-03-20 23:56:45 +0100

    finished dependencies

--- devenv/devenv.nix
+++ devenv/devenv.nix
@@ -4,31 +4,13 @@
 {
   packages = with pkgs; [
     poetry
-    terraform
     docker-compose
     kubernetes
     htop
-    unixtools.ifconfig
-    unixtools.ping
     dnslookup
     nmap
     traceroute
   ];
-  virtualisation = {
-    podman.enable = true;
-    storage-driver="overlay2";
-    docker = { 
-      enable = true; 
-      data-route = builtins.getEnv "DATA_ROUTE"
-      deamon.settings {
-        experimental = true;
-      }
-    }
-  }
-  user.extraGroups.docker.members ["miro"];
   languages.python.enable = true;
   languages.python.version = "3.12";
-  enterShell = ''
-    cd ../
-  '';
 }
