8d920cc90d32e0a9188fe36215aaf7eef48ffb64
miroaccgh <wabysaby@proton.me>
2025-03-23 21:48:02 +0100

    add final changes for evaluation

--- devenv/devenv.nix
+++ devenv/devenv.nix
@@ -2,16 +2,51 @@
 # Declarition for reproducible dev environment
 
 {
-  packages = with pkgs; [
+ packages = with pkgs; [
+    #package managment & local runtime
     poetry
+    nodejs
+    yarn
+    
+    # Terminal Language processors
+    jq # JSON
+    yq # YAML
+
+    # Search tools
+    ripgrep # fast grep
+    fd # faster find
+    # fzf # search tool
+
+    # Containerization & Orchestration
+    # docker
     docker-compose
-    kubernetes
+    kind # For running k8s clusters locally
+    kubetui # k8s interface
+    k9s # k8s TUI
+    helm # Helm packagemanager
+
+    # Networking & Debugging
     htop
-    dnslookup
+    dnsutils # Tools like: dig, dnslookup, etc.
     nmap
     traceroute
-    nodejs
+    curl
+    wget
+    netcat
+    socat
+
+    # Infrastructure & Automation
+    # terraform
+    ansible
+ #  packer # For creating VM & container images
+
+    # Monitoring & Logging
+    btop # Advanced system monitoring
+    glances # System monitoring
+    iftop # Network traffic monitoring
+    sysstat # CPU/IO statistics
   ];
-  languages.python.enable = true;
-  languages.python.version = "3.12";
-}
+ languages.python.version = "3.12";
+ languages.python.enable = true;
+ enterShell = ''../..''
+} 
